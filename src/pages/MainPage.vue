7388dc427db339fc1f7abae5e74a0b07577faa62
Egor-Babashko <egor.babashko2005@mail.ru>
2022-09-18 14:06:10 +0300

    Start Project

---
+++ src/pages/MainPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Main Page</h1>
+</template>
+
+<script>
+export default {
+  name: "MainPage",
+  components: {}
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/router/router.js
@@ -0,0 +1,16 @@
+import {createRouter, createWebHistory} from "vue-router/dist/vue-router";
+import HomePage from "@/pages/HomePage";
+
+const routes = [
+    {
+        path: '/',
+        component: HomePage
+    },
+]
+
+const router = createRouter({
+    routes,
+    history: createWebHistory(process.env.BASE_URL)
+})
+
+export default router;
